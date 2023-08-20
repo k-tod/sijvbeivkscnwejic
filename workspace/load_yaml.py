f91dc400f74bbb68ccea0a6e71759777a779d94c
Cristian Lupu <ktod_cristi@yahoo.com>
2023-08-20 09:50:53 +0200

    return UIDs instead of names

--- workspace/load_yaml.py
+++ workspace/load_yaml.py
@@ -1,9 +1,30 @@
 import yaml
 from car_model import CarModel
+from car_component import CarComponent
 
+component_dict = {}
 
 def load_yaml(file_path):
     with open(file_path, 'r') as file:
         data = yaml.safe_load(file)
+        print(data)
     car_data = data['cars']
-    return [CarModel(name=model['Car_model'], engine=model['engine'], gearbox=model['gearbox'], headlights=model['headlights'], dashboard=model['dashboard']) for model in car_data]
+    car_models = []
+    for model in car_data:
+        engine = component_dict.get(model['engine'], CarComponent(model['engine']))
+        component_dict[model['engine']] = engine
+        
+        gearbox = component_dict.get(model['gearbox'], CarComponent(model['gearbox']))
+        component_dict[model['gearbox']] = gearbox
+        
+        headlights = component_dict.get(model['headlights'], CarComponent(model['headlights']))
+        component_dict[model['headlights']] = headlights
+        
+        dashboard = component_dict.get(model['dashboard'], CarComponent(model['dashboard']))
+        component_dict[model['dashboard']] = dashboard
+        
+        car = CarModel(name=model['Car_model'], engine=engine, gearbox=gearbox, headlights=headlights, dashboard=dashboard)
+        car_models.append(car)
+    return car_models
+
+
